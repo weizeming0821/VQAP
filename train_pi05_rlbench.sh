bb59badd97bd298c4e8b2e6bf73fb5892e89d7c7
weizeming <weizeming0821@gmail.com>
2026-07-07 16:11:15 +0800

    feat: 更新 train_pi05_rlbench.sh，修改 CUDA 可见设备设置以支持更多 GPU

--- train_pi05_rlbench.sh
+++ train_pi05_rlbench.sh
@@ -9,4 +9,4 @@ export HF_HOME="${HF_HOME:-${REPO_ROOT}/.cache/huggingface}"
 export HF_DATASETS_CACHE="${HF_DATASETS_CACHE:-${REPO_ROOT}/.cache/huggingface/datasets}"
 
 
-CUDA_VISIBLE_DEVICES="2,3,6,7" torchrun --nproc_per_node=4 scripts/train_pi05_rlbench.py "$@"
+CUDA_VISIBLE_DEVICES="0,1,2,3" torchrun --nproc_per_node=4 scripts/train_pi05_rlbench.py "$@"
